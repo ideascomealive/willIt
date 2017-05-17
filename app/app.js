80270659891cbe5ccc7b0827af1485c4c55dc8fe
Michael Desantis <multishifties@yahoo.com>
2017-05-17 11:33:09 -0500

    add and configure eslint

--- app/app.js
+++ app/app.js
@@ -1,3 +1,3 @@
-var React = require("react");
-var ReactDOM = require("react-dom");
+const React = require("react");
+const ReactDOM = require("react-dom");
 ReactDOM.render(<p>Hello World!</p>, document.getElementById("app"));
